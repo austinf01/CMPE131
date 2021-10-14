c9d7cf0e9a160dafad77ca7cff340b0c04dbd7d3
Austin Fong <austin.fong@sjsu.edu>
2021-10-14 00:16:37 -0700

    fixed timing.py

--- timing.py
+++ timing.py
@@ -1,10 +1,11 @@
 import time
 
 def calculate_time(func):
-	startTime = time.time()							#retrieves time before function run
-	func()									#runs start() function
-	print ("Total time:","%.2f" % round(time.time() - startTime, 2), "seconds")	#returns time after function run - time before function run
-	return True
+	def wrapper():
+		startTime = time.time()							#retrieves time before function run
+		func()									#runs start() function
+		print ("Total time:","%.2f" % round(time.time() - startTime, 2), "seconds")	#returns time after function run - time before function run
+	return wrapper
 
 @calculate_time
 def start():
